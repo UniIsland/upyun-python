9052690158916a22ed4a05c34b0358d902af7977
Tao <tao@tukeq.com>
2012-05-03 13:50:41 +0800

    import upstream code and add simple setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='upyun-python',
+    version='1.0.1',
+    description='UpYun Python SDK',
+    author='Tao Huang',
+    author_email='tao@tukeq.com',
+    url='https://github.com/UniIsland/upyun-python',
+    packages=['upyun'],
+)
+
